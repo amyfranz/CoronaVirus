ee61a967f370dc5befef83f08d77faa76e6af516
alexandre jamarco <alexandre.j@gmail.com>
2020-03-24 16:43:00 +0000

    added the controllers

--- backend/app/controllers/ratings_controller.rb
+++ backend/app/controllers/ratings_controller.rb
@@ -1,2 +1,28 @@
 class RatingsController < ApplicationController
+    def index #list of ratings
+        ratings = Rating.all
+        render({json: ratings, include: :pastime})
+    end
+
+    def create
+        rating = Rating.new(allowed_params)
+
+        if rating.save
+          render json: rating,status: :ok
+        else
+          render json: {data:rating.errors},status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        rating = Rating.find(params[:id])
+        rating.destroy
+        render json: {status: 'SUCCESS', message:'Deleted rating', data:rating},status: :ok
+      end
+
+    private
+
+    def allowed_params
+        params.require(:rating).permit(:rate, :pastime_id)
+    end
 end
