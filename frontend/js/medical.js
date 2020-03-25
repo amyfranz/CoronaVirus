e490f5930c5be96f21568cdcb7ce580d720b7049
Amy Franz <amy.franz@gmail.com>
2020-03-25 17:51:53 +0000

    medical page

--- frontend/js/medical.js
+++ frontend/js/medical.js
@@ -9,38 +9,95 @@ const eachMedicalAdvice = medicalAdvice => {
   const title = medicalAdvice["title"];
   const content = medicalAdvice["content"];
   const imageSrc = medicalAdvice["image"];
-  const video = medicalAdvice["video"];
-  const date = medicalAdvice["created_at"];
+  const videoSrc = medicalAdvice["video"];
+  const date = new Date(medicalAdvice["created_at"]);
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
 
   const div = document.createElement("div");
-  div.class = "info-card";
+  div.className = "info-card";
 
   // const divImg = document.createElement("div");
   // div.class = "info-card-img";
 
   const divImg = document.createElement("div");
-  div.class = "info-card-img";
+  divImg.className = "info-card-img";
   const img = document.createElement("img");
   img.src = imageSrc;
   img.alt = "";
-  img.class = "info-img";
+  img.className = "info-img";
 
-  const divInfo = document.captureEvent("div");
-  divInfo.class = "info-card-img";
+  const divInfo = document.createElement("div");
+  divInfo.className = "card-info";
 
-  const divDate = document.captureEvent("div");
-  divDate.class = "card-date";
-  const spanDay = document.captureEvent("span");
-  debugger;
-  spanDay.innerText = date.getDay();
-  const spanDate = document.captureEvent("span");
-  spanDate.innerText = date.getDay();
+  const divDate = document.createElement("div");
+  divDate.className = "card-date";
+  const spanDay = document.createElement("span");
+  spanDay.innerText = weekdays[date.getDay()];
+  const spanDate = document.createElement("span");
+  spanDate.innerText = `${date.getDate()} ${date.toLocaleString("default", {
+    month: "long"
+  })} ${date.getFullYear()}`;
+
+  const heading = document.createElement("h1");
+  heading.className = "info-card-title";
+  heading.innerText = title;
+  const p = document.createElement("p");
+  p.className = "info-card-text";
+  p.innerHTML = `${content
+    .split(" ")
+    .splice(0, 20)
+    .join(" ")}...`;
+  const btn1 = document.createElement("a");
+  btn1.className = "info-card-btn";
+  btn1.href = "#";
+  btn1.innerText = "Read More";
+  divImg.append(img);
+  divDate.append(spanDay, spanDate);
+  divInfo.append(divDate, heading, p, btn1);
+  div.append(divImg, divInfo);
+  document.querySelector(".info-container").append(div);
+
+  const btn2 = document.querySelector("#back");
+
+  const h1 = document.querySelector("#title-heading");
+  h1.innerText = title;
+
+  const p2 = document.querySelector("#content");
+  p2.innerHTML = content;
+
+  const video = document.querySelector("#corona-video");
+  video.src = videoSrc;
+
+  document.querySelector(".show").style.display = "none";
+
+  btn1.addEventListener("click", () => {
+    document.querySelector(".med-info").style.display = "none";
+    document.querySelector(".show").style.display = "flex";
+  });
+  btn2.addEventListener("click", () => {
+    document.querySelector(".show").style.display = "none";
+    document.querySelector(".med-info").style.display = "flex";
+  });
 };
 
 {
-  /* <div class="info-card">
+  /*
+      <a href="#" class="info-card-btn">Back</a>
+      <h1 class="info-card-title"></h1>
+      <p class="info-card-text"></p>
+      <video src=""></video>
+  
+  <div class="info-container"> 
+  <div class="info-card">
   <div class="info-card-img">
-    <img src="images/stock-image.jpg" alt="" class="info-img" />
   </div>
   <div class="card-info">
     <div class="card-date">
@@ -64,4 +121,4 @@ const renderIndex = data => {
   data.forEach(medicalAdvice => eachMedicalAdvice(medicalAdvice));
 };
 
-fetchData("https://localhost:3000/advices").then(renderIndex);
+fetchData("http://localhost:3000/advices").then(renderIndex);
