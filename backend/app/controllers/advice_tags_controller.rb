9208cb0b21ebb41ddb285ff9dce98a4676450988
Amy Franz <amy.franz@gmail.com>
2020-03-25 15:57:09 +0000

    test

--- backend/app/controllers/advice_tags_controller.rb
+++
@@ -1,6 +0,0 @@
-class AdviceTagsController < ApplicationController
-    def index #list of advices
-        advice_tags = AdviceTag.all
-        render({json: advice_tags})
-    end
-end
--- backend/app/models/advice.rb
+++ backend/app/models/advice.rb
@@ -1,4 +1,2 @@
 class Advice < ApplicationRecord
-    has_many :advice_tags
-    has_many :tags, through: :advice_tags
 end
